69a206266a731430fc226e362b4297e9f0a9c32d
nconlisk <noecon@gmail.com>
2019-06-11 10:33:07 +0100

    Core files for website
    Core files for website created in 2014

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+                    &copy; 2014 - Noel Conlisk
+                </div>
+            </div>
+        </div>
+    </body>
+    </div>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hoskins Group</title>
+        <link href="Site.css" rel="stylesheet" />
+        <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    </head>
+    <body>
+        <div id="page">
+            
+            <div id="main">
+		<div id="header">
+                <img src="images/logo.jpg" alt="logo" align="center" width="90%" height="20%"/></td>
+               
+                <ul id="menu">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="About.html">Facilities</a></li>
+                    <li><a href="People.html">People</a></li>
+		        <li><a href="Events.html">Events</a></li>
+                    <li><a href="Publications.html">Publications</a></li>
+                    <li><a href="Contact.html">Contact</a></li>
+                    <li><a href="Links.html">Links</a></li>
+                </ul>
+		</div>
+                <div id="content">
